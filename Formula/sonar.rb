ffc439d0c434054b64b462d5026e413cb5aa56c8
Ricardo N Feliciano (FelicianoTech) <Ricardo@Feliciano.Tech>
2023-01-28 01:16:19 -0500

    Brew formula update for sonar version v0.19.1

--- Formula/sonar.rb
+++ Formula/sonar.rb
@@ -5,20 +5,20 @@
 class Sonar < Formula
   desc "A Docker utility."
   homepage "https://github.com/felicianotech/sonar"
-  version "0.19.0"
+  version "0.19.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/felicianotech/sonar/releases/download/v0.19.0/sonar-v0.19.0-macos-arm64.tar.gz"
-      sha256 "6bf38908b77ac7c8db06cc3455134fcd33f1efb4e565eec6ab635441d612e003"
+      url "https://github.com/felicianotech/sonar/releases/download/v0.19.1/sonar-v0.19.1-macos-arm64.tar.gz"
+      sha256 "635497f577764d4d343be8de94368bf30b0726b6736e988e3f7b930af2af5161"
 
       def install
         bin.install "sonar"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/felicianotech/sonar/releases/download/v0.19.0/sonar-v0.19.0-macos-amd64.tar.gz"
-      sha256 "1f76387150c4d3b2fd8a3bf5b82f2c15e0633a10c52c713965709442d8220722"
+      url "https://github.com/felicianotech/sonar/releases/download/v0.19.1/sonar-v0.19.1-macos-amd64.tar.gz"
+      sha256 "c9f5fbfb8be38a87d6f2cdfa5bf6ce4e5196504b3234fbcbc5c39f5f13824a48"
 
       def install
         bin.install "sonar"
@@ -28,16 +28,16 @@ class Sonar < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/felicianotech/sonar/releases/download/v0.19.0/sonar-v0.19.0-linux-arm64.tar.gz"
-      sha256 "81a318f754eed02d97abc06008b0cd49f9ac3cb72d41396e9536570d99ae8b45"
+      url "https://github.com/felicianotech/sonar/releases/download/v0.19.1/sonar-v0.19.1-linux-arm64.tar.gz"
+      sha256 "0c6f403b995a8d05ad781985bf19139f2fe0937aeec41cb4288f3c424c156e54"
 
       def install
         bin.install "sonar"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/felicianotech/sonar/releases/download/v0.19.0/sonar-v0.19.0-linux-amd64.tar.gz"
-      sha256 "d3e244bd9efc03e2626da2d099da504c9ac264edf81aec6850503c437270f82f"
+      url "https://github.com/felicianotech/sonar/releases/download/v0.19.1/sonar-v0.19.1-linux-amd64.tar.gz"
+      sha256 "0e94ae21cf22f340cd755f79bb0fdb766df7ee6dcfa8ce9e16ecf20c9ff7a28c"
 
       def install
         bin.install "sonar"
